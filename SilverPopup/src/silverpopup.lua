d718061bb3e8e1f63852d2d80c99aa671a0080e9
know3 <nina.shereace@gmail.com>
2017-12-13 23:07:34 +0900

    Show cumulative silvers.

--- SilverPopup/src/silverpopup.lua
+++ SilverPopup/src/silverpopup.lua
@@ -3,19 +3,27 @@ _G['ADDONS']['SILVERPOPUP'] = _G['ADDONS']['SILVERPOPUP'] or {};
 local g = _G['ADDONS']['SILVERPOPUP'];
 local acutil = require('acutil');
 
-g.sil = {};
-g.sil.style = '{@st43}{#FFCC66}{nl}';
-g.sil.mapTotal = 0;
+g.silver = {};
+g.silver.style = '{@st43}{#FFCC66}{nl}';
+g.silver.mapTotal = 0;
+g.silver.id = 900011;
 
 function SILVERPOPUP_SILVER_UPDATE(actor,evName,itemID,itemQty)
     local itemID = math.floor(itemID);
-    if itemID == 900011 then
+    if itemID == g.silver.id then
         FRAME_AUTO_POS_TO_OBJ(g.frame,session.GetMyHandle(),0,0,3,3);
         local frame = g.frame;
         frame:ShowWindow(1);
         local richtxt = frame:GetChild('siltxt');
-        g.sil.tempTotal = g.sil.tempTotal + itemQty;
-        richtxt:SetText(string.format('%s%d Silver', g.sil.style, g.sil.tempTotal));
+        g.silver.tempTotal = g.silver.tempTotal + itemQty;
+        richtxt:SetText(
+            string.format(
+                '%s%d[%d]',
+                g.silver.style,
+                g.silver.tempTotal + g.silver.mapTotal,
+                g.silver.tempTotal
+            )
+        );
         frame:StopUpdateScript("SILVERPOPUP_CLOSE_FUNC");
         frame:RunUpdateScript("SILVERPOPUP_CLOSE_FUNC",  5, 0.0, 0, 5);
     end
@@ -25,8 +33,8 @@ function SILVERPOPUP_CLOSE_FUNC()
     local frame = g.frame;
     frame:StopUpdateScript("SILVERPOPUP_CLOSE_FUNC");
     frame:ShowWindow(0);
-    g.sil.mapTotal = g.sil.mapTotal + g.sil.tempTotal;
-    g.sil.tempTotal = 0;
+    g.silver.mapTotal = g.silver.mapTotal + g.silver.tempTotal;
+    g.silver.tempTotal = 0;
 end
 
 function SILVERPOPUP_INIT()
@@ -34,8 +42,8 @@ function SILVERPOPUP_INIT()
     local siltxt = g.frame:GetChild('siltxt');
     siltxt:SetOffset(0,110);
 
-    if (g.sil.mapTotal ~= 0) then
-        CHAT_SYSTEM(string.format('%s Silver',acutil.addThousandsSeparator(g.sil.mapTotal)));
+    if (g.silver.mapTotal ~= 0) then
+        CHAT_SYSTEM(string.format('Silver %s',acutil.addThousandsSeparator(g.silver.mapTotal)));
     end
 
     local siltxt = g.frame:GetChild('siltxt');
@@ -43,8 +51,8 @@ function SILVERPOPUP_INIT()
     siltxt:SetOffset(0,60);
     siltxt:SetGravity(2,2);
 
-    g.sil.tempTotal = 0;
-    g.sil.mapTotal = 0;
+    g.silver.tempTotal = 0;
+    g.silver.mapTotal = 0;
 end
 
 function SILVERPOPUP_ON_INIT(addon,frame)
